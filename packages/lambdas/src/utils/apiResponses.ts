eb64f527e5eca6d818e6a97e68d67154c1f2e1db
Jeff Summers <jeffsummers@geniesolutions.com.au>
2021-03-21 15:46:38 +1000

    Enable CORS

--- packages/lambdas/src/utils/apiResponses.ts
+++ packages/lambdas/src/utils/apiResponses.ts
@@ -1,26 +1,36 @@
+const defaultHeaders = {
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "OPTIONS,GET,PUT,POST,DELETE",
+};
+
 const apiResponses = {
   _200: (body: { [key: string]: any }) => {
     return {
       statusCode: 200,
       body: JSON.stringify(body, null, 2),
+      headers: defaultHeaders,
     };
   },
   _201: (body: { [key: string]: any }) => {
     return {
       statusCode: 201,
       body: JSON.stringify(body, null, 2),
+      headers: defaultHeaders,
     };
   },
   _400: (body: { [key: string]: any }) => {
     return {
       statusCode: 400,
       body: JSON.stringify(body, null, 2),
+      headers: defaultHeaders,
     };
   },
   _500: (body: { [key: string]: any }) => {
     return {
       statusCode: 500,
       body: JSON.stringify(body, null, 2),
+      headers: defaultHeaders,
     };
   },
 };
